eeb04ca1b8210600ffd38345d1e95900c6f9becb
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2025-01-01 11:01:04 +0530

    created concrete states

---
+++ Assets/Scripts/State/ConcreteStates/IdleState.cs
@@ -0,0 +1,24 @@
+using StatePattern.Enemy;
+
+namespace StatePattern.State.ConcreteState
+{
+    public class IdleState : IState
+    {
+        public OnePunchManController Owner { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+
+        public void OnStateEnter()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnStateExit()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
