ff45f055d9ad4082d5251156970dc113e517288d
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2025-01-02 11:13:27 +0530

    created patrolling state

--- Assets/Scripts/Enemy/OnePunchMan/States/PatrollingState.cs
+++ Assets/Scripts/Enemy/OnePunchMan/States/PatrollingState.cs
@@ -9,21 +9,45 @@ namespace StatePattern.Enemy
         public EnemyController Owner { get; set; }
         private IStateMachine stateMachine;
 
+        private int currentPatrollingIndex = -1;
+        private Vector3 destination;
+
         public PatrollingState(IStateMachine stateMachine) => this.stateMachine = stateMachine;
 
         public void OnStateEnter()
         {
-            throw new System.NotImplementedException();
+            SetNextWaypointIndex();
+            destination = GetDestination();
+            MoveTowardsDestination();
         }
 
         void IState.Update()
         {
-            throw new System.NotImplementedException();
+            if (ReachedDestination())
+                stateMachine.ChangeState(States.IDLE);
         }
 
         public void OnStateExit()
         {
-            throw new System.NotImplementedException();
+            
         }
+
+        private void SetNextWaypointIndex()
+        {
+            if (currentPatrollingIndex == Owner.Data.PatrollingPoints.Count - 1)
+                currentPatrollingIndex = 0;
+            else
+                currentPatrollingIndex++;
+        }
+
+        private Vector3 GetDestination() => Owner.Data.PatrollingPoints[currentPatrollingIndex];
+
+        private void MoveTowardsDestination()
+        {
+            Owner.Agent.isStopped = false;
+            Owner.Agent.SetDestination(destination);
+        }
+
+        private bool ReachedDestination() => Owner.Agent.remainingDistance <= Owner.Agent.stoppingDistance;
     }
 }
